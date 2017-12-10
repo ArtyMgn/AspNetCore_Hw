31d4f29bc4832c74e2164baff957503cd89ae317
ArtyMgn <r.artem74@gmail.com>
2017-12-10 17:48:11 +0500

    started using IStudentsStorageService

--- SecondHomework/Controllers/StudentsController.cs
+++ SecondHomework/Controllers/StudentsController.cs
@@ -1,47 +1,47 @@
-﻿using System.Collections.Generic;
-using System.Linq;
+﻿using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using SecondHomework.Models;
+using SecondHomework.Services;
 
 namespace SecondHomework.Controllers
 {
     public class StudentsController : Controller
-    { 
-        public IActionResult Index()
+    {
+        private readonly IStudentsStorageService studentsStorage;
+
+        public StudentsController(IStudentsStorageService service)
         {
-            var students = LoadStudentsFromFile("students.txt");
-            var studentsOrderedById = students.Values.OrderBy(student => student.Id);
+            studentsStorage = service;
+        }
+        
+        public IActionResult Index([FromServices]IStudentsStorageService service)
+        {
+            var studentsOrderedById = service.GetAllStudents().OrderBy(student => student.Id);
             return View(studentsOrderedById);
         }
 
         [HttpPost]
         public IActionResult Add(Student newStudent)
         {
-            var students = LoadStudentsFromFile("students.txt");
-            
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
-            
-            if (students.ContainsKey(newStudent.Id))
-            {
-                return BadRequest($"student with id = {newStudent.Id} already exist");
-            }
 
-            students.Add(newStudent.Id, newStudent);
+            studentsStorage.AddStudent(newStudent);
             var serializedStudent = JsonConvert.SerializeObject(newStudent);
-            SaveStudentsToFile(students);
-            return Content($"Added {serializedStudent}, Students count: {students.Values.Count}");
+            
+            return Content($"Added {serializedStudent}");
         }
 
         [HttpDelete]
         public IActionResult Delete(int studentId)
         {
-            var students = LoadStudentsFromFile("students.txt");
-            students.Remove(studentId);
-            SaveStudentsToFile(students);
+            var storageService =
+                ActivatorUtilities.CreateInstance<SqlStudentsStorageService>(HttpContext.RequestServices);
+            storageService.DeleteStudent(studentId);
             return Content($"Deleted student with id {studentId}");
         }
         
@@ -53,49 +53,25 @@ namespace SecondHomework.Controllers
         [HttpPost]
         public IActionResult Update(Student student)
         {
-            var students = LoadStudentsFromFile("students.txt");
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
             }
             
-            if (!students.ContainsKey(student.Id))
-            {
-                return BadRequest($"Can't update: student with id = {student.Id} doesn't exist");
-            }
-
-            students[student.Id] = student;
-            SaveStudentsToFile(students);
+            studentsStorage.Update(student);
             return RedirectToAction("Read", new {studentId = student.Id});
         }
 
         [HttpGet]
         public IActionResult Read(int studentId)
         {
-            var students = LoadStudentsFromFile("students.txt");
-
-            if (!students.ContainsKey(studentId))
+            var storageService = HttpContext.RequestServices.GetService<IStudentsStorageService>();
+            var student = storageService.FindStudentById(studentId);
+            if (student is null)
             {
                 return BadRequest($"Can't read: student with id = {studentId} doesn't exist");
             }
-                
-            return View(students[studentId]);
-        }
-
-        [NonAction]
-        public Dictionary<int, Student> LoadStudentsFromFile(string filePath)
-        {
-            if(!System.IO.File.Exists("students.txt"))
-                return new Dictionary<int, Student>();
-            var fileContent = System.IO.File.ReadAllText("students.txt");
-            return JsonConvert.DeserializeObject<Dictionary<int, Student>>(fileContent);
-        }
-
-        [NonAction]
-        public void SaveStudentsToFile(Dictionary<int, Student> students)
-        {
-            var serializedStudents = JsonConvert.SerializeObject(students);
-            System.IO.File.WriteAllText("students.txt", serializedStudents);
+            return View(student);
         }
     }
 }
